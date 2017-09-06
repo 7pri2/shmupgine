99d1d89ba8245989e005f55250ac345d7513bff5
Cyprien Eymond Laritaz <cyprien@lambdalab.fr>
2017-09-06 09:05:39 +0200

    Ajout des canaux de sons pour soundmanager

--- include/soundmanager.h
+++ include/soundmanager.h
@@ -1,23 +1,33 @@
 #ifndef __SOUNDMANAGER_H__
 #define __SOUNDMANAGER_H__
 
+#include <SFML/Audio.hpp>
 #include <map>
 #include "singleton.h"
 
+/* Solution non optimisee temporaire */
+typedef struct {
+	sf::SoundBuffer m_buffer;
+	sf::Sound		m_sound;
+} sound;
+
+
+/* On utilise un tableau de 'canaux' sonores pour jouer des sons simultanement; 
+ * tant qu'un son est en cours de lecture, on passe au canal suivant jusqu'a en
+ * trouver un libre et on le joue */
 class soundmanager : public singleton<soundmanager> {
 	friend class singleton<soundmanager>;
 public:
-	soundmanager();
-	~soundmanager();
-
-	void new_sound(std::string sname, std::string path);
+	void	new_sound(std::string sname, std::string path);
+	void	play_sound(std::string sname);
 
 private:
 	soundmanager(const soundmanager&) {};
 	soundmanager();
 	~soundmanager();
 
-	std::map<std::string, sf::Sound*> m_sounds;
+	std::map<std::string, std::string>	m_soundbank;
+	sound	m_sound_channels[256];
 };
 
 #endif
--- src/soundmanager.cpp
+++ src/soundmanager.cpp
@@ -9,7 +9,15 @@ soundmanager::~soundmanager() {
 }
 
 void soundmanager::new_sound(std::string sname, std::string path) {
-	sf::Sound* s;
-	s.loadFromFile(path);
-	m_sounds[sname] = s;
+	m_soundbank[sname] = path;
+}
+
+void soundmanager::play_sound(std::string sname) {
+	for(int i = 0; i < 256; ++i)
+		if(m_sound_channels[i].m_sound.getStatus() != sf::SoundSource::Status::Playing)
+			if(m_sound_channels[i].m_buffer.loadFromFile(m_soundbank[sname])) {
+				m_sound_channels[i].m_sound.setBuffer(m_sound_channels[i].m_buffer);
+				m_sound_channels[i].m_sound.play();
+				break;
+			}
 }
